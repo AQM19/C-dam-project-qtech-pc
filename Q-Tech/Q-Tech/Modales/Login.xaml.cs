bd548e4255786fa8fb9d91aa651e22a74b7371b9
AQM19 <aaron.quintanalmartin@iesmiguelherrero.com>
2023-03-31 07:34:43 +0200

    Actualizacion Login 2, Dashboard 1

--- Q-Tech/Q-Tech/Modales/Login.xaml.cs
+++ Q-Tech/Q-Tech/Modales/Login.xaml.cs
@@ -20,14 +20,61 @@ namespace Q_Tech
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _showPassword;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _showPassword = false;
+        }
+
+        private bool ValidData()
+        {
+            if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                MessageBox.Show("El campo usuario no puede estar vacío.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtUser.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(PasswordBind.Password))
+            {
+                MessageBox.Show("El campo contraseña no puede estar vacío.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                _ = (PasswordBind.Visibility == Visibility.Visible) ? PasswordBind.Focus() : PasswordUnbind.Focus();
+                return false;
+            }
+            return true;
         }
 
         private void PasswordViewer_Click(object sender, RoutedEventArgs e)
         {
+            _showPassword = !_showPassword;
+            PasswordUnbind.Visibility = _showPassword ? Visibility.Visible : Visibility.Hidden;
+            PasswordBind.Visibility = _showPassword ? Visibility.Hidden : Visibility.Visible;
+        }
 
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidData())
+            {
+                MessageBox.Show("¡Login correcto!.");
+            }
+        }
+
+        private void brdUser_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtUser.Focus();
+        }
+
+        private void PasswordBind_KeyUp(object sender, KeyEventArgs e)
+        {
+            PasswordUnbind.Text = PasswordBind.Password;
+        }
+
+        private void PasswordUnbind_KeyUp(object sender, KeyEventArgs e)
+        {
+            PasswordBind.Password = PasswordUnbind.Text;
         }
     }
 }
